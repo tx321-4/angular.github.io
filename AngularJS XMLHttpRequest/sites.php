b606b7784bb4f4bb0f38cde23472d37cd8ca4899
shi_yi <linyizhongupup@163.com>
2018-09-07 17:53:43 +0800

    AngularJS XMLHttpRequest

---
+++ AngularJS XMLHttpRequest/sites.php
@@ -0,0 +1,24 @@
+{{
+    "sites" : [
+            {
+                "Name" : "菜鸟教程",
+                "Url" : "www.runoob.com",
+                "Country" : "CN"
+            },
+            {
+                "Name" : "Google",
+                "Url" : "www.google.com",
+                "Country" : "USA"
+            },
+            {
+                "Name" : "Fackbook",
+                "Url" : "www.fackbook.com",
+                "Country" : "USA"
+            },
+            {
+                "Name" : "微博",
+                "Url" : "www.weibo.com",
+                "Country" : "CN"
+            }
+        ]
+}}
